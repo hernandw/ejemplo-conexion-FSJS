8fe61d1164be63568b00d39e50b71802d3f934bd
Williams Hernandez <hernandw@gmail.com>
2024-04-04 22:58:27 -0300

    cambios

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
 const { Pool } = require('pg');
+require('dotenv').config()
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'empresadb',
-    password: '123456',
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
     port: 5432
 })
 
